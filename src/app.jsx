d532f15dd5040dd1cfdf7769e0c8c70d144ec0d1
Norman Kuo <Norman.h.kuo@gmail.com>
2023-03-01 15:50:48 -0800

    First React Project

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./component/navbar"
+import Main from "./component/main"
+
+function App() {
+    return(
+        <div className="Container">    
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default App
---
+++ src/component/navbar.jsx
@@ -0,0 +1,13 @@
+
+// import Image from "../images/react-icon-small.png"
+
+function NavBar () {
+    return(
+    <nav>
+        <img src='/public/react-icon-small.png' className="nav--icon"/>
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>)
+}
+
+export default NavBar
